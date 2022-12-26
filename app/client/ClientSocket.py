0a7441102309f07add56221c6d04bbce008b918e
Ales Kirsch <kirschales@gmail.com>
2022-12-26 17:47:52 +0100

    Socket prototypes now sends bytes, not string

--- app/client/ClientSocket.py
+++ app/client/ClientSocket.py
@@ -4,5 +4,5 @@ HOST = 'localhost'
 PORT = 5000
 
 client_socket = socket.create_connection((HOST, PORT))
-client_socket.send('Hello World!')
+client_socket.send(b'Hello World!')
 client_socket.close()
