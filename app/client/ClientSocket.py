358d7463dcf3e09c027bd6417d4cab996cb11ae8
Ales Kirsch <kirschales@gmail.com>
2022-12-26 20:18:12 +0100

    Put client and server into main functions

--- app/client/ClientSocket.py
+++ app/client/ClientSocket.py
@@ -4,6 +4,12 @@ from ..common.ConnectedSocket import ConnectedSocket
 HOST = 'localhost'
 PORT = 5000
 
-client_socket = ConnectedSocket(socket.create_connection((HOST, PORT)))
-client_socket.send('Hello World!')
-client_socket.close()
+
+def main():
+    client_socket = ConnectedSocket(socket.create_connection((HOST, PORT)))
+    client_socket.send('Hello World!')
+    client_socket.close()
+
+
+if __name__ == "__main__":
+    main()
--- app/server/ServerSocket.py
+++ app/server/ServerSocket.py
@@ -5,13 +5,18 @@ HOST = 'localhost'
 PORT = 5000
 
 
-if socket.has_dualstack_ipv6():
-    server_socket = socket.create_server((HOST, PORT), family=socket.AF_INET6, dualstack_ipv6=True)
-else:
-    server_socket = socket.create_server((HOST, PORT))
+def main():
+    if socket.has_dualstack_ipv6():
+        server_socket = socket.create_server((HOST, PORT), family=socket.AF_INET6, dualstack_ipv6=True)
+    else:
+        server_socket = socket.create_server((HOST, PORT))
 
-server_socket.listen()
-conn, addr = server_socket.accept()
-conn = ConnectedSocket(conn)
-msg = conn.recv(1024).decode()
-print(msg)
+    server_socket.listen()
+    conn, addr = server_socket.accept()
+    conn = ConnectedSocket(conn)
+    msg = conn.recv(1024).decode()
+    print(msg)
+
+
+if __name__ == '__main__':
+    main()
