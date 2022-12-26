23ea7e3f1bb4364599d963ca91cd89632af90af7
Ales Kirsch <kirschales@gmail.com>
2022-12-26 20:07:32 +0100

    Client now uses ConnectedSocket

--- app/client/ClientSocket.py
+++ app/client/ClientSocket.py
@@ -1,8 +1,9 @@
 import socket
+from ..common.ConnectedSocket import ConnectedSocket
 
 HOST = 'localhost'
 PORT = 5000
 
-client_socket = socket.create_connection((HOST, PORT))
-client_socket.send(b'Hello World!')
+client_socket = ConnectedSocket(socket.create_connection((HOST, PORT)))
+client_socket.send('Hello World!')
 client_socket.close()
