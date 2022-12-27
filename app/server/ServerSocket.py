4198de678d593a3ad840535a945c723a1d39f095
Ales Kirsch <kirschales@gmail.com>
2022-12-28 00:26:47 +0100

    Made ServerSocket persistent, through buggy

--- app/server/ServerSocket.py
+++ app/server/ServerSocket.py
@@ -1,8 +1,16 @@
 import socket
+from .database.database import Database
 from ..common.ConnectedSocket import ConnectedSocket
 
 HOST = 'localhost'
 PORT = 5000
+BUFFER_SIZE = 4096
+COMMAND_SIZE = 3
+
+PUT = b'PUT'
+GET = b'GET'
+OK = 'OK'
+db = Database()
 
 
 def main():
@@ -12,10 +20,24 @@ def main():
         server_socket = socket.create_server((HOST, PORT))
 
     server_socket.listen()
-    conn, addr = server_socket.accept()
-    conn = ConnectedSocket(conn)
-    msg = conn.recv(1024).decode()
-    print(msg)
+
+    try:
+        while True:
+            conn = ConnectedSocket(server_socket.accept()[0])
+            request_type = conn.recv(3)
+            if request_type == GET:
+                conn.send(OK)
+                conn.send('\n')
+                conn.send(db.update())
+            elif request_type == PUT:
+                db.insert(conn.recv(2048).decode())
+                conn.send(OK)
+                conn.send('\n')
+            else:
+                conn.send('BAD\n')
+            conn.close()
+    except KeyboardInterrupt:
+        print("Ending Server.")
 
 
 if __name__ == '__main__':
