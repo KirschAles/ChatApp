b0ac85acdfc77833245d059fbbedf1e0bf9140e5
Ales Kirsch <kirschales@gmail.com>
2022-12-30 14:49:50 +0100

    Changed table structure

--- app/server/database/scripts/CREATE.sql
+++ app/server/database/scripts/CREATE.sql
@@ -8,6 +8,7 @@ CREATE TABLE IF NOT EXISTS usser (
     password VARCHAR(40)
 );
 ALTER TABLE usser ADD CONSTRAINT pk_user PRIMARY KEY (user_id);
+ALTER TABLE usser ADD CONSTRAINT un_username UNIQUE (username);
 
 CREATE TABLE IF NOT EXISTS user_chat (
     user_id INTEGER NOT NULL,
@@ -22,8 +23,12 @@ REFERENCES chat (chat_id) ON DELETE CASCADE;
 CREATE TABLE IF NOT EXISTS message (
     message_id SERIAL NOT NULL,
     chat_id INTEGER NOT NULL,
+    time_send TIMESTAMP,
     message VARCHAR(1024)
+    sender_id INTEGER NOT NULL
 );
 ALTER TABLE message ADD CONSTRAINT pk_message PRIMARY KEY (message_id);
 ALTER TABLE message ADD CONSTRAINT fk_message FOREIGN KEY (chat_id)
 REFERENCES chat (chat_id) ON DELETE CASCADE;
+ALTER TABLE message ADD CONSTRAINT fk_message_sender FOREIGN KEY (sender_id)
+REFERENCES usser (user_id) ON DELETE CASCADE;
