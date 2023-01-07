4d55c029e902241558765c5405e10dcdf8876db0
Ales Kirsch <kirschales@gmail.com>
2023-01-07 20:22:55 +0100

    Made request overclass for operation which need loggin

---
+++ app/common/request/serverrequests/logged.py
@@ -0,0 +1,13 @@
+from app.common.request.serverrequest import ServerRequest
+from app.server.database.safedb import SafeDatabase
+from typing import TextIO
+import app.common.headers as headers
+
+
+class LoggedRequest(ServerRequest):
+    headers_needed = []
+
+    def __init__(self, reader: TextIO, db: SafeDatabase):
+        self.super(self).__init__(reader, db)
+        if not db.is_password_right(self.headers[headers.USERNAME], self.headers[headers.PASSWORD]):
+            raise ValueError('Wrong username or password.')
