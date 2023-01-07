976b20de93a0ab36c0db3fde297b7f1b9a64fbf3
Ales Kirsch <kirschales@gmail.com>
2023-01-07 20:05:52 +0100

    Implemented register execution

---
+++ app/common/request/serverrequests/register.py
@@ -0,0 +1,15 @@
+from app.common.request.serverrequest import ServerRequest
+from app.server.database.safedb import SafeDatabase
+from typing import TextIO
+import app.common.headers as headers
+
+
+class RegisterRequest(ServerRequest):
+    headers_needed = []
+
+    def __init__(self, reader: TextIO, db: SafeDatabase):
+        self.super(self).__init__(reader, db)
+
+    def execute(self):
+        self.db.create_user(self.headers[headers.USERNAME], self.headers[headers.PASSWORD])
+
