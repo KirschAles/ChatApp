1c3332a5f64352c6ac06d2c44b9220b3fe32f3a5
Ales Kirsch <kirschales@gmail.com>
2023-01-07 20:24:38 +0100

    Now loggedrequests are used

--- app/common/request/serverrequests/sendmessage.py
+++ app/common/request/serverrequests/sendmessage.py
@@ -1,10 +1,10 @@
-from app.common.request.serverrequest import ServerRequest
+from app.common.request.serverrequests.logged import LoggedRequest
 from app.server.database.safedb import SafeDatabase
 from typing import TextIO
 import app.common.headers as headers
 
 
-class SendMessageRequest(ServerRequest):
+class SendMessageRequest(LoggedRequest):
     headers_needed = [headers.CHAT_ID, headers.CONTENT_LENGTH]
 
     def __init__(self, reader: TextIO, db: SafeDatabase):
