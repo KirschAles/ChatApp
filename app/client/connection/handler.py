a0841089d8c525aec51f7560509cc2941623edad
Ales Kirsch <kirschales@gmail.com>
2023-01-08 15:20:29 +0100

    Made connection handler

---
+++ app/client/connection/handler.py
@@ -0,0 +1,38 @@
+import socket
+
+from app.common.request.request import Request
+from app.common.response.ClientResponse import ClientResponse
+from app.common.headers import CONTENT_LENGTH, USERNAME, PASSWORD
+
+HOST = 'localhost'
+PORT = 5000
+username = ''
+password = ''
+
+
+def handle_response(response: ClientResponse):
+    response.build_message()
+    response.headers[CONTENT_LENGTH] = len(response.final_message)
+    response.headers[USERNAME] = username
+    response.headers[PASSWORD] = password
+    response.build_header_strings()
+    conn = socket.create_connection((HOST, PORT))
+    writer = conn.makefile('rw', encoding='utf8')
+    response.add_writer(writer)
+    response.send_response()
+
+    request = Request(writer)
+    request.build_request()
+    request.read_message()
+    conn.close()
+    return request
+
+
+def main():
+    msg = input()
+    while msg != 'exit()':
+        client_socket = ConnectedSocket(socket.create_connection((HOST, PORT)))
+        client_socket.send(msg)
+        print(client_socket.recv(2048))
+        client_socket.close()
+        msg = input()
