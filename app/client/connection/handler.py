b3b75165a3d8c45264b01d88c84a0cb1635a3932
Ales Kirsch <kirschales@gmail.com>
2023-01-08 15:21:12 +0100

    Delteted useless part

--- app/client/connection/handler.py
+++ app/client/connection/handler.py
@@ -26,13 +26,3 @@ def handle_response(response: ClientResponse):
     request.read_message()
     conn.close()
     return request
-
-
-def main():
-    msg = input()
-    while msg != 'exit()':
-        client_socket = ConnectedSocket(socket.create_connection((HOST, PORT)))
-        client_socket.send(msg)
-        print(client_socket.recv(2048))
-        client_socket.close()
-        msg = input()
