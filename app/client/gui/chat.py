a880150a5b1fe88211cc9cd8edc8b3cf0253662f
Ales Kirsch <kirschales@gmail.com>
2023-02-16 21:29:05 +0100

    Implemented chat gui

--- app/client/gui/chat.py
+++ app/client/gui/chat.py
@@ -10,6 +10,23 @@ class ChatBox(QWidget):
         self.back = QPushButton('Back')
         self.setLayout(QVBoxLayout())
         self.layout().addWidget(self.back)
+        self.chat_window = QLabel()
+        self.load_chat()
+        self.layout().addWidget(self.chat_window)
+        self.send_button = QPushButton('Send')
+        self.send_button.clicked.connect(self.send_message)
+        self.layout().addWidget(self.send_button)
+        self.message = QLineEdit()
+        self.layout().addWidget(self.message)
+
+    def load_chat(self):
+        for message in self.chat.messages:
+            self.chat_window.setText(self.chat_window.text() + str(message) + '\n')
+
+    def send_message(self):
+        text = self.message.text()
+        self.chat.add_message(text, self.username)
+        self.chat_window.setText(self.chat_window.text() + str(self.chat.last_message) + '\n')
 
 
 
