359187a97fdfece1b8addb49b0dd50e62c34a328
Ales Kirsch <kirschales@gmail.com>
2023-02-16 20:01:59 +0100

    Remade gui connections

--- app/client/controller.py
+++ app/client/controller.py
@@ -1,17 +1,32 @@
 import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication, QLineEdit, QPushButton, QLabel, QVBoxLayout, QWidget
 from app.client.gui.conversations import Conversations
+from app.client.gui.chat import ChatBox
+from app.client.database.conversations import ConvDB
 
 
+class MainWindow(QMainWindow):
+    def __init__(self, conv_db):
+        super(MainWindow, self).__init__()
+        self.conv_db = conv_db
+        self.conversations = Conversations(self.conv_db)
+        self.start_conversations_window()
 
+    def start_conversations_window(self):
+        self.conversations = Conversations(self.conv_db)
+        self.setCentralWidget(self.conversations)
+        self.conversations.chat_buttons.buttonClicked.connect(self.start_chat_window)
+
+    def start_chat_window(self, button):
+        chat_id = self.conversations.chat_buttons.id(button)
+        chat = ChatBox(self.conv_db[chat_id])
+        self.setCentralWidget(chat)
+        chat.back.clicked.connect(self.start_conversations_window)
 
 
 app = QApplication(sys.argv)
 
-window = QMainWindow()
-w = QWidget()
-window.setCentralWidget(w)
-
-w.setLayout(Conversations())
+conv_db = ConvDB()
+window = MainWindow(conv_db)
 window.show()
 app.exec()
--- app/client/gui/conversations.py
+++ app/client/gui/conversations.py
@@ -1,29 +1,33 @@
-from PyQt5.QtWidgets import QVBoxLayout, QPushButton, QTextEdit
+from PyQt5.QtWidgets import QVBoxLayout, QPushButton, QTextEdit, QButtonGroup, QWidget
 from app.client.database.conversations import ConvDB
 
 
 # temporary counter of conversation id, will be later replaced with request to server
 
 
-class Conversations(QVBoxLayout):
+class Conversations(QWidget):
     tmp_counter = 0
 
     def __init__(self, conv_db: ConvDB = ConvDB()):
         super().__init__()
+        self.setLayout(QVBoxLayout())
         self._db = conv_db
         self._conv_widgets = {}
         add_conv = QPushButton('Add conversation')
         add_conv.clicked.connect(self.new_conversation)
         self.username_field = QTextEdit()
-        self.addWidget(add_conv)
-        self.addWidget(self.username_field)
+        self.layout().addWidget(add_conv)
+        self.layout().addWidget(self.username_field)
+        self.chat_buttons = QButtonGroup()
 
     def new_conversation(self):
         self._db[self.tmp_counter] = [self.username_field.toPlainText()]
         self.username_field.setText('')
         self.update_widget(self.tmp_counter)
-        self.addWidget(self._conv_widgets[self.tmp_counter])
+        self.layout().addWidget(self._conv_widgets[self.tmp_counter])
         self.tmp_counter += 1
 
     def update_widget(self, id: int):
-        self._conv_widgets[self.tmp_counter] = QPushButton(str(self._db[self.tmp_counter].users))
+        button = QPushButton(str(self._db[self.tmp_counter].users))
+        self.chat_buttons.addButton(button, self.tmp_counter)
+        self._conv_widgets[self.tmp_counter] = button
