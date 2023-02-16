ac949bae688ad53cb646519ea4511f38d0dcd4fe
Ales Kirsch <kirschales@gmail.com>
2023-02-16 20:01:30 +0100

    Remade chat gui prototype

--- app/client/gui/chat.py
+++ app/client/gui/chat.py
@@ -1,28 +1,14 @@
-from PyQt5.QtWidgets import QLineEdit, QPushButton, QLabel, QVBoxLayout
-from app.common.response.ClientResponse import ClientResponse
-import app.common.servercommands as cmd
-import app.common.headers as headers
-from app.client.connection.handler import handle_response
+from PyQt5.QtWidgets import QLineEdit, QPushButton, QLabel, QVBoxLayout, QWidget
+from app.client.database.chat import ChatDB
 
 
-class ChatBox(QVBoxLayout):
-    def __init__(self, chat_id):
+class ChatBox(QWidget):
+    def __init__(self, chat: ChatDB):
         super().__init__()
-        self.chat_id = chat_id
-        self.chat = QLabel()
-        self.chat.setWordWrap(True)
-        self.chat_write = QLineEdit()
-        self.chat_send = QPushButton("SEND")
-        self.chat_send.clicked.connect(self.send_message)
-        self.addWidget(self.chat)
-        self.addWidget(self.chat_send)
-        self.addWidget(self.chat_write)
+        self.chat = chat
+        self.back = QPushButton('Back')
+        self.setLayout(QVBoxLayout())
+        self.layout().addWidget(self.back)
 
-    def send_message(self):
-        original_text = self.chat.text()
-        self.chat_write.setText('')
-        response = ClientResponse([original_text])
-        response.command = cmd.SEND_MESSAGE
-        response.headers[headers.CHAT_ID] = self.chat_id
-        request = handle_response(response)
+
 
