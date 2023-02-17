204b4d0a5b7c45bc55e5e63d6a474b1c4c601be9
Ales Kirsch <kirschales@gmail.com>
2023-02-17 17:45:49 +0100

    Client gui now starts with the login screen

--- app/client/controller.py
+++ app/client/controller.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import QMainWindow, QApplication, QLineEdit, QPushButton, Q
 from app.client.gui.conversations import Conversations
 from app.client.gui.chat import ChatBox
 from app.client.database.conversations import ConvDB
+from app.client.gui.login import Login
 
 
 class MainWindow(QMainWindow):
@@ -10,7 +11,8 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         self.conv_db = conv_db
         self.conversations = Conversations(self.conv_db)
-        self.start_conversations_window()
+        self.login = None
+        self.start_login_window()
 
     def start_conversations_window(self):
         self.conversations = Conversations(self.conv_db)
@@ -22,6 +24,17 @@ class MainWindow(QMainWindow):
         chat = ChatBox(self.conv_db[chat_id], self.conv_db.username)
         self.setCentralWidget(chat)
         chat.back.clicked.connect(self.start_conversations_window)
+
+    def login_user(self):
+        username = self.login.username.text()
+        password = self.login.password.text()
+        self.conv_db = ConvDB(username, password)
+        self.start_conversations_window()
+
+    def start_login_window(self):
+        self.login = Login()
+        self.setCentralWidget(self.login)
+        self.login.login.clicked.connect(self.login_user)
 
 
 app = QApplication(sys.argv)
