eb532e04354c5a52a34c6fa4dab6e5365d190b34
Ales Kirsch <kirschales@gmail.com>
2023-05-22 04:47:27 +0200

    Response will now not fail, if mesage has length zero

--- app/common/request/response.py
+++ app/common/request/response.py
@@ -11,10 +11,11 @@ class Response(ClientResponse):
 
     def build_message(self):
         long_message = ''
-        for part in self.message[0: len(self.message) - 1]:
-            long_message += str(part)
-            long_message += '+'
-        long_message += self.message[-1]
+        if len(self.message) != 0:
+            for part in self.message[0: len(self.message) - 1]:
+                long_message += str(part)
+                long_message += '+'
+            long_message += self.message[-1]
         self.headers[headers.CONTENT_LENGTH] = len(long_message)
         self.final_message = long_message
 
