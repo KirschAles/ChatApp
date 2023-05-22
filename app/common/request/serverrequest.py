3f6a071f0e90523265d5b14d8e8e39ed23bf3c86
Ales Kirsch <kirschales@gmail.com>
2023-05-22 06:08:16 +0200

    Will now send header info gotten in execute phase

--- app/common/request/serverrequest.py
+++ app/common/request/serverrequest.py
@@ -16,6 +16,8 @@ class ServerRequest(request.Request):
         self.return_message = []
         self.db = db
         self.build_request()
+
+        self.headers_to_send = {}
         if not self.has_all_headers():
             raise ValueError('Header missing.')
 
@@ -31,5 +33,6 @@ class ServerRequest(request.Request):
     def send_response(self):
         response = Response(self.reader, self.return_message)
         response.command = OK
+        response.headers = self.headers_to_send.copy()
         response.build_response()
         response.send_response()
